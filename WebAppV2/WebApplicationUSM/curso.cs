9ebd33334b1d2a630fa69baed0afa5f374de60dd
Nivek- <kevin.gutierrez.p@gmail.com>
2014-12-02 17:09:36 -0300

    visual

---
+++ WebAppV2/WebApplicationUSM/curso.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationUSM
+{
+    public class curso
+    {
+       
+        public string Cod_curso { get; set; }
+      
+
+        public curso()
+        {
+            
+            this.Cod_curso = "";
+           
+
+        }
+        public curso( string cod_curso)
+        {
+          
+            this.Cod_curso = cod_curso;
+            
+
+
+        }
+    }
+}
---
+++ WebAppV2/WebApplicationUSM/profesor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationUSM
+{
+    public class profesor
+    {
+         public string Cod { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Pass { get; set; }
+
+        public profesor()
+        {
+            this.Cod = "";
+            this.Nombre = "";
+            this.Pass = "";
+        }
+
+
+
+        public profesor(string cod, string nombre, string apellido, string pass)
+        {
+            this.Cod = cod;
+            this.Nombre = nombre;
+            this.Apellido = Apellido;
+            this.Pass = pass;
+        }
+    }
+}
